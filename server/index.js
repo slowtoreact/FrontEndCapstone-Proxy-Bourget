d25ad49f19ccdf3e2fc84e80cbfa93d8ce861efe
Jared Bourget <jared.m.bourget@gmail.com>
2019-07-17 13:45:17 -0600

    Renders my component

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const server = express();
 
+
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,19 +15,30 @@ const port = 5000;
 
 
 server.get('./bundle.js/:3001', (req, res) => {
-    request('http://localhost:3002/bundle.js', (err, response, body) => {
-        res.status(200).send(body);
-    })
+  request('http://localhost:3001/bundle.js', (err, response, body) => {
+    res.status(200).send(body);
+  })
 })
 
 server.use('/api/cities', (req, res)=> {
-    request(`http://localhost:3001/api/cities`, (error, response, body) => {
-        if(response.statusCode === 200) {
-            res.status(200).send(body);
-        }
+  request(`http://localhost:3001/api/cities`, (error, response, body) => {
+        // console.log(body)
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+      }
+    })
+})
+
+server.get('/restaurant', (req, res)=> {
+  request(`http://localhost:3001/restaurant/`, (error, response, body) => {
+       console.log()
+    if(response.statusCode === 200) {
+          console.log(req)
+      res.status(200).send(body);
+      }
     })
 })
 
 server.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 })
