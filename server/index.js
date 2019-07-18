9e340e3198634b0b492091d52096660548d8ffec
Jared Bourget <jared.m.bourget@gmail.com>
2019-07-17 19:35:58 -0600

    A lot of hard-coded styling

--- server/index.js
+++ server/index.js
@@ -2,25 +2,23 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 const bodyParser = require('body-parser');
-
 const server = express();
-
 
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: true}));
-
 server.use(express.static('../client'));
+
 
 const port = 5000;
 
 
 server.get('./bundle.js/:3001', (req, res) => {
-  request('http://localhost:3001/bundle.js', (err, response, body) => {
+  request('http://localhost:3001/bundle.js', (error, response, body) => {
     res.status(200).send(body);
   })
 })
 
-server.use('/api/cities', (req, res)=> {
+server.use('/api/cities', (req, res) => {
   request(`http://localhost:3001/api/cities`, (error, response, body) => {
         // console.log(body)
     if(response.statusCode === 200) {
@@ -29,11 +27,12 @@ server.use('/api/cities', (req, res)=> {
     })
 })
 
-server.get('/restaurant', (req, res)=> {
-  request(`http://localhost:3001/restaurant/`, (error, response, body) => {
-       console.log()
+server.use('/restaurant', (req, res) => {
+  console.log(req.query.name)
+  request(`http://localhost:3001/restaurant?name=${req.query.name}`, (error, response, body) => {
+       console.log(error)
     if(response.statusCode === 200) {
-          console.log(req)
+          // console.log(response)
       res.status(200).send(body);
       }
     })
